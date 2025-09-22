5884e01d7a5a592d6fb890e72a74ff9ac8868ad0
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-09-22 11:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+      const squareList = document.querySelectorAll(".square");
+
+      function onHoverChange(index) {
+        for (let i = 0; i < squareList.length; i++) {
+          if (i !== index) {
+            squareList[i].classList.add("coffee");
+          }
+        }
+      }
+
+      function reset() {
+        for (let i = 0; i < squareList.length; i++) {
+          squareList[i].classList.remove("coffee");
+        }
+      }
+
+      for (let i = 0; i < squareList.length; i++) {
+        squareList[i].addEventListener("mouseover", () => {
+          onHoverChange(i);
+        });
+
+        squareList[i].addEventListener("mouseout", () => {
+          reset();
+        });
+      }
